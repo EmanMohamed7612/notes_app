1c64203996b8d7d2c06071741110c7dcb39c46a7
Eman Mohamed <emanm0024@gmail.com>
2025-10-21 16:11:13 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_views.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.dark(), home:const NotesView());
+  }
+}
