12bb9c742a3b7f0cc16f401f8e86abb81c159f66
Eman Mohamed <emanm0024@gmail.com>
2025-11-27 01:14:36 +0200

    edit note done

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/edit_view_body.dart';
 
 class EditNoteView extends StatelessWidget {
-  const EditNoteView({super.key});
-
+  const EditNoteView({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: CustomAppBar(title: 'Edit Note', icon: Icons.check),
-      ),
-      body: EditNoteViewBody(),
+      // appBar: AppBar(
+      //   title: CustomAppBar(
+      //     onPressed: (){},
+      //     title: 'Edit Note', icon: Icons.check),
+      // ),
+      body: EditNoteViewBody(note: note),
     );
   }
 }
